3ef697571ce1aaae710e4b2307ffd6e51636e9c5
CODE-NXTDOOR <jsantoine24@gmail.com>
2024-12-18 21:37:45 -0600

    code revision lts

--- Terraform_project_2/Foundation/main.tf
+++ Terraform_project_2/Foundation/main.tf
@@ -5,30 +5,38 @@ resource "aws_default_vpc" "default-vpc" {
 }
 
 data "aws_vpc" "default-vpc" {
-    id = var.default_vpc
+  id = var.default_vpc
 }
 
 data "aws_subnets" "public_subnets" {
-    filter {
-      name = "vpc-id"
-      values = [var.default_vpc]
-    }
+  filter {
+    name   = "vpc-id"
+    values = [var.default_vpc]
+  }
 }
 
 # Retrieve all Subnet IDs in the Default VPC
 data "aws_subnets" "default_vpc_subnets" {
   filter {
     name   = "vpc-id"
-    values = aws_default_vpc.default-vpc.id
+    values = [aws_default_vpc.default-vpc.id]
   }
 }
 
 # Creating subnets for the default VPC
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Default subnet for us-east-1"
+    Name = "Default subnet for us-east-1a"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Default subnet for us-east-1b"
   }
 }
 
@@ -39,11 +47,11 @@ data "aws_availability_zones" "available" {
 
 # Creating an Autoscaling Group
 resource "aws_autoscaling_group" "terraform_asg" {
-  name                = "my-terraform-asg"
-  min_size            = var.min_size
-  max_size            = var.max_size
-  desired_capacity    = var.desired_capacity
-  availability_zones  = var.availability_zones
+  name               = "my-terraform-asg"
+  min_size           = var.min_size
+  max_size           = var.max_size
+  desired_capacity   = var.desired_capacity
+  availability_zones = var.availability_zones
 
   launch_template {
     id      = aws_launch_template.launch-asg.id
@@ -64,7 +72,7 @@ resource "aws_launch_template" "launch-asg" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key
-  user_data     = file("install_apache.sh")
+  user_data     = base64encode(file("install_apache.sh")) # Explicit encoding of user data
 
   network_interfaces {
     associate_public_ip_address = true
@@ -73,6 +81,9 @@ resource "aws_launch_template" "launch-asg" {
 
   tag_specifications {
     resource_type = "instance"
+    tags = {
+      Name = "MyLaunchASGInstance"
+    }
   }
 }
 
